19a35e35906288ae224ac031e5b2fda7a2271036
Edgar Mikuro <edgarmikuro@gmail.com>
2025-04-13 14:45:51 +0300

    Completes fetch lab

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+    // Step 1: Fetch data from the Game of Thrones API
+    return fetch("https://anapioficeandfire.com/api/books")
+    // Step 2: Convert the response to JSON
+    .then(response => response.json())
+    // Step 3: Pass the book data to renderBooks to display on the page
+    .then(bookData => renderBooks(bookData));
   
 }
 
